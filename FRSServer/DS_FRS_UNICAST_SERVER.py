5b97190ac07b7f9a662fa84a7152b494da607f04
albimema <albimema@gmail.com>
2022-01-29 19:30:54 +0100

    Create DS_FRS_UNICAST_SERVER.py

---
+++ FRSServer/DS_FRS_UNICAST_SERVER.py
@@ -0,0 +1,44 @@
+import os
+
+class DS_FRS_unicast_Server():
+    def __init__(self,buffer_size):
+        pass
+        self.buffer_size=buffer_size
+
+    def main(self,conn,ip,port):
+        self.ip=ip
+        self.port=port
+
+        while(True):
+            data=conn.recv(self.buffer_size)
+            print(f"Received: {data}")
+            txt=str(data)
+
+            if txt=="Avaliable?":
+                conn.send("Yes!")
+
+            elif txt.startwith("SIZE"):
+                image_size=txt.split()[1]
+                self.image_size=image_size
+            
+            elif data:
+                image=open("temp.png",'wb')
+                image_bits=conn.recv(self.buffer_size)
+                while(image_bits):
+                    image.write(image_bits)
+                    image_bits=conn.recv(self.buffer_size)
+                image.close()
+                local_image_size=os.path.getsize("temp.png")
+                if local_image_size==self.image_size:
+                    conn.send("Image received! Contacting UPS!")
+                    break
+                else:
+                    conn.send("Image corrupted! Send again!")
+                    continue
+
+                        
+
+
+
+
+
