16ddae5c76b1bc999ed531d4a0d738f2911e8a40
albimema <albimema@gmail.com>
2022-02-06 22:55:44 +0100

    test2

--- DSMachine/exceptions.py
+++ DSMachine/exceptions.py
@@ -3,8 +3,15 @@ exceptions
 """
 
 
-class ServerUnreachableException(Exception):
+class FRSUnreachableException(Exception):
 	"""
 	thrown when one of the servers can no longer be reached by the client
 	"""
+	pass
+
+class ASUnreachableException(Exception):
+	pass
+
+
+class UPSTimeOut(Exception):
 	pass
